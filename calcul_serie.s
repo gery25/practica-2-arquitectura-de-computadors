d6442c46052c60b0669473fb5e6ca997d9668e6b
gery25 <gerymaps200515@gmail.com>
2025-05-05 14:27:10 +0200

    update calcul_series
    actualitzacio del codi fallo en el ordre de una comanda i retabuacio per a que quedi mes coerent

--- calcul_serie.s
+++ calcul_serie.s
@@ -1,48 +1,43 @@
 .data 
 resultat: .double 0.0   ; Variable per guardar el resultat en punt flotant
-
+dos: .double 2.0       ; Constant 2.0
+tres: .double 3.0      ; Constant 3.0
+quatre: .double 4.0    ; Constant 4.0
+cinc: .double 5.0      ; Constant 5.0
+ene: .double 0.0       ; Constant 0.0
 .text
-main:   daddi r1, r0, 0     ; Inicialitzem a 0 el comptador
-        daddi r3, r0, 5     ; Inicialitzem a 5 el valor limit del bucle 
+main:   
+    daddi r1, r0, 0     ; Inicialitzem a 0 el comptador
+    daddi r3, r0, 5     ; Inicialitzem a 5 el valor limit del bucle 
 
-        ; Convertim les constants a punt flotant
-        daddi r4, r0, 2     
-        mtc1 r4, f2         ; Movem 2 a f2
-        cvt.l.d f2, f2      ; Convertim a double
+    ; Carreguem les constants en registres de punt flotant
+    l.d f2, dos(r0)     ; Carreguem 2.0
+    l.d f3, tres(r0)    ; Carreguem 3.0
+    l.d f4, quatre(r0)  ; Carreguem 4.0
+    l.d f5, cinc(r0)    ; Carreguem 5.0
 
-        daddi r4, r0, 3
-        mtc1 r4, f3         ; Movem 3 a f3
-        cvt.l.d f3, f3      ; Convertim a double
+loop:   
+    mtc1 f1, r1        ; Movem n a f1
+    cvt.l.d f1, f1      ; Convertim n a double
+    mul.d f6, f2, f1    ; 2 * n
+    add.d f6, f6, f3    ; 2n + 3 
 
-        daddi r4, r0, 4
-        mtc1 r4, f4         ; Movem 4 a f4
-        cvt.l.d f4, f4      ; Convertim a double
+    ; Calculem n³
+    mul.d f7, f1, f1    ; n²
+    mul.d f7, f7, f1    ; n³
+    mul.d f7, f7, f3    ; 3n³
 
-        daddi r4, r0, 5
-        mtc1 r4, f5         ; Movem 5 a f5
-        cvt.l.d f5, f5      ; Convertim a double
+    ; Calculem 4n i sumem
+    mul.d f8, f4, f1    ; 4n
+    add.d f7, f7, f8    ; 3n³ + 4n
+    add.d f7, f7, f5    ; 3n³ + 4n + 5
 
-    loop:   mtc1 r1, f1         ; Movem n a f1
-            cvt.l.d f1, f1      ; Convertim n a double
-            mul.d f6, f2, f1    ; 2 * n
-            add.d f6, f6, f3    ; 2n + 3 
-
-            ; Calculem n³
-            mul.d f7, f1, f1    ; n²
-            mul.d f7, f7, f1    ; n³
-            mul.d f7, f7, f3    ; 3n³
-
-            ; Calculem 4n i sumem
-            mul.d f8, f4, f1    ; 4n
-            add.d f7, f7, f8    ; 3n³ + 4n
-            add.d f7, f7, f5    ; 3n³ + 4n + 5
-
-            ; Fem la divisió
-            div.d f10, f6, f7   ; (2n + 3)/(3n³ + 4n + 5)
+    ; Fem la divisió
+    div.d f10, f6, f7   ; (2n + 3)/(3n³ + 4n + 5)
 
     ; Incrementar comptador i comprovar
     daddi r1, r1, 1     ; Incrementem n
-    bne r1, r3, loop   ; Si r1 != 5, tornem al bucle
+    bne r1, r3, loop    ; Si r1 != 5, tornem al bucle
 
     s.d f10, resultat(r0) ; Guardem el resultat final
     halt                ; Fi del programa
---
+++ winmips64.las
@@ -0,0 +1,1 @@
+Z:\home\gerard\Escritorio\clase\2n curs\2n semestre\arquitectura\prac2\practica-2-arquitectura-de-computadors\calcul_serie.s
